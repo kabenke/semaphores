b698221d3c8a81c4c08c1f52a8b6d5a2263a1a0a
Oyoki <51206941+kabenke@users.noreply.github.com>
2021-09-17 20:30:24 +0200

    release and acquire implement

--- Simple.java
+++ Simple.java
@@ -1,13 +1,40 @@
+import java.util.concurrent.Semaphore;
 public class Simple
 {
-  public static void main (String[] args)
-  {
-  
+  // Initialise 3 Semaphores
+  private final static Semaphore S1 = new Semaphore(0);
+  private final static Semaphore S2 = new Semaphore(0);
+  private final static Semaphore S3 = new Semaphore(0);
+
+
+  public static void main(String[] args) {
+  try{
+    f1();
+    f2();
+    f3();
+  } catch (InterruptedException e) {
+    e.printStackTrace();
+  }
   }
 
-  public f1()
-    
-    public f2()
-    
-    public f3()
+  public static void f1() throws InterruptedException {
+    System.out.println("2");
+    S1.acquire();
+    S3.release();
+    System.out.println("13");
+  }
+
+  public static void f2() throws InterruptedException {
+    S1.release();
+    System.out.println("3");
+    System.out.println("5");
+    S2.release();
+  }
+
+  public static void f3() throws InterruptedException {
+    S2.acquire();
+    System.out.println("7");
+    System.out.println("11");
+    S3.release();
+  }
 }
